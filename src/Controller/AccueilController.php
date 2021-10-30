751193d2b68c4fdfb474d84f951a1df092b63f98
tophe <tophe@LAPTOP-FLENNB3D>
2021-10-30 15:45:44 +0200

    Phase 4 : plusieurs pages.

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,19 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * Description of AccueilController
  *
  * @author tophe
  */
-class AccueilController {
+class AccueilController extends AbstractController {
+     
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
     public function index(): Response {
-        return new Response("Hello world!");
+        return $this->render("pages/accueil.html.twig");
+        //return new Response($this->twig->render("pages/accueil.html.twig"));
+        //return new Response("Hello world!");
     }
 }
