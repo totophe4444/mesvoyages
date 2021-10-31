d911353e75578a198d22d0105e52d8f131471e8b
tophe <tophe@LAPTOP-FLENNB3D>
2021-10-31 11:05:45 +0100

    Phase 16 : ORM (gestion des environnements : ajout et suppression).

---
+++ src/Controller/admin/AdminEnvironnementController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller\admin;
+
+use App\Entity\Environnement;
+use App\Form\VisiteType;
+use App\Repository\EnvironnementRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * Description of AdminEnvironnementController
+ *
+ * @author tophe
+ */
+class AdminEnvironnementController extends AbstractController {
+   /**
+     *
+     * @var EntityManagerInterface 
+     */
+    private $om;
+    /**
+     *
+     * @var EnvironnementRepository 
+     */
+    private $repository;
+    function __construct(EnvironnementRepository $repository, EntityManagerInterface $om) {
+        $this->repository = $repository;
+        $this->om = $om;
+    }
+    /**
+     * @Route("/admin/environnements", name="admin.environnements")
+     * @return Response
+     */
+    public function index(): Response {
+        $environnements = $this->repository->findAll();
+        return $this->render("admin/admin.environnements.html.twig",[
+            'environnements' => $environnements
+        ]);
+    } 
+    /**
+     * @Route("/admin/environnements/suppr/{id}", name="admin.environnement.suppr")
+     * @param Environnement $environnement
+     * @return Response
+     */
+    public function suppr(Environnement $environnement): Response {
+        $this->om->remove($environnement);
+         $this->om->flush();
+        return $this->redirectToRoute('admin.environnements');
+    } 
+    /**
+     * @Route("/admin/environnement/ajout", name="admin.environnement.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request): Response {
+        $nomEnvironnement=$request->get("nom");
+        $environnement = new Environnement();
+        $environnement->setNom($nomEnvironnement);
+        $this->om->persist($environnement);
+        $this->om->flush();
+        return $this->redirectToRoute('admin.environnements');
+    } 
+}
